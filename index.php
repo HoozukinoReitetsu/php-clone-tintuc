9211b943bd7cbe4f47d964fd6d94eed4e1138de5
TienTho98 <50016575+HoozukinoReitetsu@users.noreply.github.com>
2020-02-28 17:55:12 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,23 +1,38 @@
 <?php 
     include('simple_html_dom.php');
-    //dường dẫn cần lấy tin
-    $url = 'https://vnexpress.net/the-thao';
+    $conn=mysqli_connect('localhost','root','','tin tức');
+    if (!$conn) {
+    exit('Kết nối không thành công!');
+}
+    // thành công
+    echo 'Kết nối thành công!';
+    // //dường dẫn cần lấy tin
+    $url = 'https://vnexpress.net';
     $html = file_get_html($url);    
-    //tìm theo thẻ
+    // //tìm theo thẻ
     foreach ($noidung = $html->find('.title_news a') as $e) {
         $a = array();
-        //lấy thuộc tính của thẻ
+        // lấy thuộc tính của thẻ
         $url = $e->href;
-        $html = file_get_html($url);
-        $a[]=$e->innertext;        
+        $html = file_get_html($url); 
+        $a[]=addslashes($e->plaintext);     
         $a[]=str_replace("https://vnexpress.net","http://localhost",$e->href);
-        // echo '</br>';
-        // echo $e->href;
-        // echo "</br>";
+        $a[1]=str_replace("#box_comment","",$a[1]); 
+        $a[0]=chop($a[0]);     
+        echo "<br>" .$a[0];
        foreach ($noidung = $html->find('article.content_detail.fck_detail.width_common.block_ads_connect') as $e2) {
            $a[]=$e2->innertext;
-           echo "</br>";
        }
-      print_r($a);
+       if($a[0]!=""){
+        $sql="INSERT INTO `tintuc`(`title`, `href`, `decription`) VALUES ('$a[0]','$a[1]','$a[2]')";
+         unset($a);
+       }
+          if (mysqli_query($conn, $sql)) {
+             
+      echo "New record created successfully";
+} else {
+    //   echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
     }
+    mysqli_close($conn);
     ?>
